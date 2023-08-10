c166dd071eb513c59916a3241215f1ee5dd32b7e
Serkan IŞIK <dehnas79@gmail.com>
2023-08-10 15:06:04 +0300

    DbContext and DbSet

---
+++ EFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=EfCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
--- EFCore.DatabaseFirst/Program.cs
+++ EFCore.DatabaseFirst/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EFCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    /*
+    foreach (var item in products)
+    {
+        Console.WriteLine($"{item.Id} - {item.Name} - {item.Price}");
+    }
+    */
+    
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Price}");
+    });
+}
+
+Console.ReadLine();
