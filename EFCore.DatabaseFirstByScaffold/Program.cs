00eaca68f650f903bee80291e88647c98c3d3534
Serkan IŞIK <dehnas79@gmail.com>
2023-08-12 12:08:47 +0300

    Add code to retrieve and display Product data

--- EFCore.DatabaseFirstByScaffold/Program.cs
+++ EFCore.DatabaseFirstByScaffold/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EFCore.DatabaseFirstByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var dbContext = new EfCoreDatabaseFirstDbContext()) {
+    
+    var products = await dbContext.Products.ToListAsync();
+
+    /*
+    foreach (var item in products)
+    {
+        Console.WriteLine($"{item.Id} - {item.Name} - {item.Price}");
+    }
+    */
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock}");
+    });
+}
+
+Console.ReadLine();
