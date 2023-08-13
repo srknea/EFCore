7ffc3854fef64bab59bd4f20baa79454e38efa06
Serkan IŞIK <dehnas79@gmail.com>
2023-08-13 11:06:52 +0300

    Create AddBarcode_ForProduct migration

--- EFCore.CodeFirst/DAL/Product.cs
+++ EFCore.CodeFirst/DAL/Product.cs
@@ -13,5 +13,6 @@ namespace EFCore.CodeFirst.DAL
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string? Description { get; set; }
+        public string Barcode { get; set; }
     }
 }
--- EFCore.CodeFirst/Migrations/20230813074931_AddBarcode_ForProduct.Designer.cs
+++ EFCore.CodeFirst/Migrations/20230813074931_AddBarcode_ForProduct.Designer.cs
@@ -3,6 +3,7 @@ using EFCore.CodeFirst.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCore.CodeFirst.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230813074931_AddBarcode_ForProduct")]
+    partial class AddBarcode_ForProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
