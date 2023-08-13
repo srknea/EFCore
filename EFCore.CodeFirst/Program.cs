77c5dd54fd86901af530f2cbb40de693c19026e2
Serkan IŞIK <dehnas79@gmail.com>
2023-08-13 08:56:51 +0300

    Create appsettings.json and DbContextInitializer

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    /*
+    foreach (var item in products)
+    {
+        Console.WriteLine($"{item.Id} - {item.Name} - {item.Price}");
+    }
+    */
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock}");
+    });
+}
+
+Console.ReadLine();
