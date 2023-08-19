ecff7d1bdc993f32461a779996d441336f8db007
Serkan IŞIK <dehnas79@gmail.com>
2023-08-19 13:18:59 +0300

    Updating untracked data

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -5,16 +5,14 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var product = await _context.Products.FirstAsync();
+    var product = new Product() { Id = 2, Name = "Pilot Kalem", Price = 1, Stock = 1, Description = null, Barcode = "A-0020-Z" };
+
     Console.WriteLine($"First state: {_context.Entry(product).State}");
 
-    _context.Entry(product).State = EntityState.Detached;
-    
-    Console.WriteLine($"Second state: {_context.Entry(product).State}");
-    
-    product.Price = 1;
 
-    Console.WriteLine($"Last state: {_context.Entry(product).State}");
+    _context.Update(product);
+
+    Console.WriteLine($"Second state: {_context.Entry(product).State}");
 
 
     await _context.SaveChangesAsync();
