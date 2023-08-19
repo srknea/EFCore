0d24283247af7c7250cb6a62d0029bea07396bdc
Serkan IŞIK <dehnas79@gmail.com>
2023-08-19 12:54:51 +0300

    EntityState.Detached

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -6,19 +6,18 @@ DbContextInitializer.Build();
 using (var _context = new AppDbContext())
 {
     var product = await _context.Products.FirstAsync();
-    
     Console.WriteLine($"First state: {_context.Entry(product).State}");
 
-
-    _context.Entry(product).State = EntityState.Deleted;
-
-    //_context.Remove(product);
+    _context.Entry(product).State = EntityState.Detached;
+    
+    Console.WriteLine($"Second state: {_context.Entry(product).State}");
+    
+    product.Price = 1;
 
     Console.WriteLine($"Last state: {_context.Entry(product).State}");
-    
-    
-    await _context.SaveChangesAsync();
 
+
+    await _context.SaveChangesAsync();
     Console.WriteLine($"State after save changes: {_context.Entry(product).State}");
 }
 
