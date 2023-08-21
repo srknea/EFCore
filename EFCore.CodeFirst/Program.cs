86d8dfe2ab48b4828f70918ac11c7db5920be624
Serkan IŞIK <dehnas79@gmail.com>
2023-08-21 13:39:05 +0300

    AsNoTracking method

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -5,18 +5,14 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var product = new Product() { Id = 2, Name = "Pilot Kalem", Price = 1, Stock = 1, Description = null, Barcode = "A-0020-Z" };
+    
+    var products = await _context.Products.AsNoTracking().ToListAsync();
 
-    Console.WriteLine($"First state: {_context.Entry(product).State}");
-
-
-    _context.Update(product);
-
-    Console.WriteLine($"Second state: {_context.Entry(product).State}");
-
-
-    await _context.SaveChangesAsync();
-    Console.WriteLine($"State after save changes: {_context.Entry(product).State}");
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock} - {p.Barcode} ---> {_context.Entry(p).State}");
+    });
+    
 }
 
 Console.ReadLine();
