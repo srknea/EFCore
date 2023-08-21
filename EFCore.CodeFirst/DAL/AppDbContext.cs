ddfda662fc05b5b28c7b0f8b9860d65506d3e3db
Serkan IŞIK <dehnas79@gmail.com>
2023-08-21 15:22:22 +0300

    DbContext: ChangeTracker property

--- EFCore.CodeFirst/DAL/AppDbContext.cs
+++ EFCore.CodeFirst/DAL/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace EFCore.CodeFirst.DAL
     public class AppDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -6,13 +6,46 @@ DbContextInitializer.Build();
 using (var _context = new AppDbContext())
 {
     
-    var products = await _context.Products.AsNoTracking().ToListAsync();
+    var products = await _context.Products.ToListAsync();
+    var customers = await _context.Customers.ToListAsync();
 
     products.ForEach(p =>
     {
         Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock} - {p.Barcode} ---> {_context.Entry(p).State}");
     });
-    
+
+    Console.WriteLine("- - - - - - - - - - -");
+
+    customers.ForEach(c =>
+    {
+        Console.WriteLine($"{c.Id} - {c.Name} - {c.City} - {c.Email} ---> {_context.Entry(c).State}");
+    });
+
+    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
+
+    _context.ChangeTracker.Entries().ToList().ForEach(e =>
+    {
+        Console.WriteLine($"{e.Entity.GetType().Name} - {e.State}"); 
+    });
+
+    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+    _context.ChangeTracker.Entries().ToList().ForEach(e =>
+    {
+        if (e.Entity is Product p)
+        {
+            Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock} - {p.Barcode} ---> {_context.Entry(p).State}");
+        }
+        else if (e.Entity is Customer c)
+        {
+            Console.WriteLine($"{c.Id} - {c.Name} - {c.City} - {c.Email} ---> {_context.Entry(c).State}");
+
+        }
+    });
+
 }
 
 Console.ReadLine();
+
+
+
