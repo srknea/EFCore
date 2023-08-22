10b65f6562caf7808ee1e3f7fa4c6b9050a65607
Serkan IŞIK <dehnas79@gmail.com>
2023-08-22 20:30:59 +0300

    Add Category and Product entity

---
+++ EFCore.CodeFirst.Relationships/DAL/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst.Relationships.DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int Barcode { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
