94d5dcb304b3c24a2e29044ba50d97eb4f915df2
Serkan IŞIK <dehnas79@gmail.com>
2023-08-23 16:38:23 +0300

    Shadow Property - 2

--- EFCore.CodeFirst.Relationships/DAL/Category.cs
+++ EFCore.CodeFirst.Relationships/DAL/Category.cs
@@ -8,7 +8,16 @@ namespace EFCore.CodeFirst.Relationships.DAL
 {
     public class Category
     {
+        public Category()
+        {
+            Products = new List<Product>(); // Initialize the Products list in the constructor
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<Product> Products { get; set;}
+        
+        //public ICollection<Product> Products { get; set; }
     }
 }
