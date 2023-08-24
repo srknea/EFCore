43ae095a43eca517c2ec89d026855466d62dc9b9
Serkan IŞIK <dehnas79@gmail.com>
2023-08-24 14:45:24 +0300

    Add Data - 2

--- EFCore.CodeFirst.Relationships.OneToMany.DataAdd/Program.cs
+++ EFCore.CodeFirst.Relationships.OneToMany.DataAdd/Program.cs
@@ -4,12 +4,14 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var category = new Category() { Name = "Kalemler" };
+    var category = new Category() { Name = "Defterler" };
 
-    var product = new Product() { Name = "Pilot Kalem", Price = 12.50m, Stock = 100, Barcode = 123456789, Category = category };
+    category.Products.Add(new Product() { Name = "Defter1", Price = 12.50m, Stock = 100, Barcode = 123456789 });
+    category.Products.Add(new Product() { Name = "Defter2", Price = 12.50m, Stock = 100, Barcode = 123455623 });
+    category.Products.Add(new Product() { Name = "Defter3", Price = 12.50m, Stock = 100, Barcode = 123565237 });
 
-    _context.Products.Add(product);
-
+    _context.Categories.Add(category);
+    
     _context.SaveChanges();
 }
 
