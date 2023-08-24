6c7ad9e3809462dff56d0c1552ac80622453d25b
Serkan IŞIK <dehnas79@gmail.com>
2023-08-24 14:34:28 +0300

    Add Category and Product entity

--- EFCore.CodeFirst.Relationships.OneToMany.DataAdd/DAL/AppDbContext.cs
+++ EFCore.CodeFirst.Relationships.OneToMany.DataAdd/DAL/AppDbContext.cs
@@ -10,6 +10,8 @@ namespace EFCore.CodeFirst.Relationships.OneToMany.DataAdd.DAL
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             DbContextInitializer.Build();
