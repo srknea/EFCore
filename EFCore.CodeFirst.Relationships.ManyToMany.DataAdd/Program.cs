f64c1bf435475570b4ed6ca810f1fcf6d1622540
Serkan IŞIK <dehnas79@gmail.com>
2023-08-24 22:24:59 +0300

    Data Add - 1

--- EFCore.CodeFirst.Relationships.ManyToMany.DataAdd/Program.cs
+++ EFCore.CodeFirst.Relationships.ManyToMany.DataAdd/Program.cs
@@ -4,7 +4,14 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
+    var student = new Student(){ Name = "Serkan", Age = 12};
 
+    student.Teachers.Add(new Teacher() { Name = "Murat"});
+    student.Teachers.Add(new Teacher() { Name = "Sevgül"});
+
+    _context.Students.Add(student); 
+
+    _context.SaveChanges();
 }
 
 Console.ReadKey();
