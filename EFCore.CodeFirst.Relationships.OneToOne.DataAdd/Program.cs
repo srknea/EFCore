2e87470c79322f61bc9c925fdcf13e3658f8ab4a
Serkan IŞIK <dehnas79@gmail.com>
2023-08-24 17:32:04 +0300

    Add Data - 1

--- EFCore.CodeFirst.Relationships.OneToOne.DataAdd/Program.cs
+++ EFCore.CodeFirst.Relationships.OneToOne.DataAdd/Program.cs
@@ -4,7 +4,11 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
+    var product = new Product{ Name = "Pilot Kalem", Price = 10.5m, Stock = 10, Barcode = 120321549};
  
+    _context.Products.Add(product);
+
+    _context.SaveChanges();
 }
 
 Console.ReadKey();
