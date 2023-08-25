76a98388d4a03fc71bea38e5419aa6b40f4481e8
Serkan IŞIK <dehnas79@gmail.com>
2023-08-25 09:15:11 +0300

    DeleteBehavior.Cascade

--- EFCore.CodeFirst.Relationships.DeleteBehaviors/Program.cs
+++ EFCore.CodeFirst.Relationships.DeleteBehaviors/Program.cs
@@ -4,18 +4,10 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var category = new Category()
-    {
-        Name = "Kalemler",
-        Products = new List<Product>()
-        {
-        new Product () {Name = "Kalem1", Price = 10, Stock = 10, Barcode = 987},
-        new Product () {Name = "Kalem2", Price = 10, Stock = 10, Barcode = 988},
-        new Product () {Name = "Kalem3", Price = 10, Stock = 10, Barcode = 989}
-        }
-    };
+    var category = _context.Categories.First();
 
-    _context.Categories.Add(category);
+    _context.Categories.Remove(category);
+
     _context.SaveChanges();
 }
 
