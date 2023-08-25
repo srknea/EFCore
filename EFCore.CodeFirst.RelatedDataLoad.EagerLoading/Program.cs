8c4689058e6f3bb689a203beb0d9642b7bf4903e
Serkan IŞIK <dehnas79@gmail.com>
2023-08-25 18:57:52 +0300

    Data Add

--- EFCore.CodeFirst.RelatedDataLoad.EagerLoading/Program.cs
+++ EFCore.CodeFirst.RelatedDataLoad.EagerLoading/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore.CodeFirst.RelatedDataLoad.EagerLoading.DAL;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var category = new Category() {Name = "Kalemler"};
+
+    category.Products.Add(new Product() 
+    {
+        Name = "Pilot Kalem",
+        Price = 12.50m,
+        Stock = 100,
+        Barcode = 123456789,
+        ProductFeature = new ProductFeature() 
+            { 
+            Width = 10, Height = 20, Color = "Mavi" 
+            }
+    });
+
+    category.Products.Add(new Product()
+    {
+        Name = "Kurşun Kalem",
+        Price = 40.50m,
+        Stock = 200,
+        Barcode = 123456789,
+        ProductFeature = new ProductFeature()
+        {
+            Width = 10,
+            Height = 20,
+            Color = "Siyah"
+        }
+    });
+
+    await _context.AddAsync(category);
+
+    await _context.SaveChangesAsync();
+}
+
+Console.ReadKey();
