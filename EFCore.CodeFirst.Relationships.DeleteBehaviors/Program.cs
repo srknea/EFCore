4542dc4e36eb735e9f393bd8a7aff0a718a938e5
Serkan IŞIK <dehnas79@gmail.com>
2023-08-25 09:11:14 +0300

    Data Add

--- EFCore.CodeFirst.Relationships.DeleteBehaviors/Program.cs
+++ EFCore.CodeFirst.Relationships.DeleteBehaviors/Program.cs
@@ -4,7 +4,19 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    
+    var category = new Category()
+    {
+        Name = "Kalemler",
+        Products = new List<Product>()
+        {
+        new Product () {Name = "Kalem1", Price = 10, Stock = 10, Barcode = 987},
+        new Product () {Name = "Kalem2", Price = 10, Stock = 10, Barcode = 988},
+        new Product () {Name = "Kalem3", Price = 10, Stock = 10, Barcode = 989}
+        }
+    };
+
+    _context.Categories.Add(category);
+    _context.SaveChanges();
 }
 
 Console.ReadKey();
