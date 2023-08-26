d7bf81c52136b7c76b0353f27016874bcd188c07
Serkan IŞIK <dehnas79@gmail.com>
2023-08-26 12:47:54 +0300

    Lazy Loading - 1

--- EFCore.CodeFirst.RelatedDataLoad.LazyLoading/Program.cs
+++ EFCore.CodeFirst.RelatedDataLoad.LazyLoading/Program.cs
@@ -4,8 +4,13 @@ DbContextInitializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    
+    Console.WriteLine("Loading categories...\n");
 
+    var category = _context.Categories.First();
+
+    Console.WriteLine("\nLoading products...\n");
+
+    var products = category.Products;
 }
 
 Console.ReadKey();
