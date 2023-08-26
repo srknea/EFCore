5d1c01dd60b87b8e66b137be4fa45ef7bf5e3d88
Serkan IŞIK <dehnas79@gmail.com>
2023-08-26 11:50:57 +0300

    Create EFCore.CodeFirst.RelatedDataLoad.LazyLoading

---
+++ EFCore.CodeFirst.RelatedDataLoad.LazyLoading/DAL/ProductFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst.RelatedDataLoad.LazyLoading.DAL
+{
+    public class ProductFeature
+    {
+        [ForeignKey("Product")]
+        public int Id { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Color { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
