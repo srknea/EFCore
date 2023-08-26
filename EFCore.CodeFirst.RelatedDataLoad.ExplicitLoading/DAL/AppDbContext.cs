5fda4e7dd1ab29e8d7af142636ea782595c06fd5
Serkan IŞIK <dehnas79@gmail.com>
2023-08-26 09:09:23 +0300

    Create EFCore.CodeFirst.RelatedDataLoad.ExplicitLoading

---
+++ EFCore.CodeFirst.RelatedDataLoad.ExplicitLoading/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst.RelatedDataLoad.ExplicitLoading.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DbContextInitializer.Build();
+
+            optionsBuilder.UseSqlServer(DbContextInitializer.Configuration.GetConnectionString("SqlCon"));
+        }
+    }
+}
---
+++ EFCore.CodeFirst.RelatedDataLoad.ExplicitLoading/Program.cs
@@ -0,0 +1,42 @@
+﻿using EFCore.CodeFirst.RelatedDataLoad.ExplicitLoading.DAL;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var category = new Category() { Name = "Kalemler" };
+
+    category.Products.Add(new Product()
+    {
+        Name = "Pilot Kalem",
+        Price = 12.50m,
+        Stock = 100,
+        Barcode = 123456789,
+        ProductFeature = new ProductFeature()
+        {
+            Width = 10,
+            Height = 20,
+            Color = "Mavi"
+        }
+    });
+
+    category.Products.Add(new Product()
+    {
+        Name = "Kurşun Kalem",
+        Price = 40.50m,
+        Stock = 200,
+        Barcode = 123456789,
+        ProductFeature = new ProductFeature()
+        {
+            Width = 10,
+            Height = 20,
+            Color = "Siyah"
+        }
+    });
+
+    await _context.AddAsync(category);
+
+    await _context.SaveChangesAsync();
+}
+
+Console.ReadKey();
